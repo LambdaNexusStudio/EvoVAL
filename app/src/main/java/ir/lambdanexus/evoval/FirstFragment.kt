bf42c63c99b6fb220c32d117563fc2b38cc931b3
Ali Nazer <89677246+aL1NaZ4R@users.noreply.github.com>
2023-04-07 15:49:06 +0330

    [no caption]

--- app/src/main/java/ir/lambdanexus/evoval/FirstFragment.kt
+++ app/src/main/java/ir/lambdanexus/evoval/FirstFragment.kt
@@ -1,31 +1,30 @@
 package ir.lambdanexus.evoval
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.myapplication.R
-import com.example.myapplication.databinding.AskBinding
+import ir.lambdanexus.evoval.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: AskBinding? = null
+    private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
 
-        _binding = AskBinding.inflate(inflater, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,11 +32,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonYes.setOnClickListener {
-            findNavController().navigate(R.id.action_firstFragment_to_gay_istrue)
-        }
-        binding.buttonNo.setOnClickListener {
-            findNavController().navigate(R.id.action_firstFragment_to_gay_isfalse)
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 
--- app/src/main/java/ir/lambdanexus/evoval/SecondFragment.kt
+++ app/src/main/java/ir/lambdanexus/evoval/SecondFragment.kt
@@ -1,20 +1,19 @@
 package ir.lambdanexus.evoval
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.myapplication.R
-import com.example.myapplication.databinding.FragmentAskIstrueBinding
+import ir.lambdanexus.evoval.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class ask_istrue : Fragment() {
+class SecondFragment : Fragment() {
 
-    private var _binding: FragmentAskIstrueBinding? = null
+    private var _binding: FragmentSecondBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +24,7 @@ class ask_istrue : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentAskIstrueBinding.inflate(inflater, container, false)
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,8 +32,8 @@ class ask_istrue : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonOk.setOnClickListener {
-            findNavController().navigate(R.id.action_gay_istrue_to_firstFragment)
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
 
